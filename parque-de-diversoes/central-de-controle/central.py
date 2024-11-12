f2346df53fa2d0d08efe9a667defbaa3e8575a16
Ederson Torresini <etorresini@ifsc.edu.br>
2024-11-12 10:12:26 -0300

    Simplificar código

--- parque-de-diversoes/central-de-controle/central.py
+++ parque-de-diversoes/central-de-controle/central.py
@@ -4,13 +4,12 @@ from re import compile
 from os import getenv
 
 load_dotenv()
-mqtt_host = getenv("MQTT_HOST", default="mqtt-broker")
-mqtt_port = getenv("MQTT_PORT", default=1883)
+mqtt_host = getenv("MQTT_HOST", default="test.mosquitto.org")
 mqtt_topic = getenv("MQTT_TOPIC", default="itl20242/atualizar")
-mqtt_keepalive = getenv("MQTT_KEEPALIVE", default=60)
 
 
 def on_connect(client, userdata, flags, reason_code, properties):
+    print("Conectado ao broker MQTT")
     client.subscribe(mqtt_topic + "/#")
 
 
@@ -26,5 +25,5 @@ mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
-mqtt_client.connect(mqtt_host, mqtt_port, mqtt_keepalive)
+mqtt_client.connect(mqtt_host)
 mqtt_client.loop_forever()
