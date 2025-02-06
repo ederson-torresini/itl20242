36bb4960bfb0a3bf6e93251058871807b31faa05
Ederson Torresini <etorresini@ifsc.edu.br>
2025-02-06 16:51:55 -0300

    Melhorar interface com ícones de parque

--- parque-de-diversoes/painel-de-controle/parque.js
+++ parque-de-diversoes/painel-de-controle/parque.js
@@ -1,87 +1,114 @@
 export default class parque extends Phaser.Scene {
-  constructor () {
-    super('parque')
+  constructor() {
+    super("parque");
+
+    this.desligar = undefined;
+    this.ligar = undefined;
   }
 
-  preload () {
-    this.load.audio('sino', 'assets/sino.mp3')
-    this.load.spritesheet('a', 'assets/a.png', {
+  preload() {
+    this.load.audio("sino", "assets/sino.mp3");
+
+    this.load.image("barco-viking", "assets/barco-viking.png");
+    this.load.image("carrossel", "assets/carrossel.png");
+    this.load.image("montanha-russa", "assets/montanha-russa.png");
+
+    this.load.spritesheet("desligar", "assets/desligar.png", {
       frameWidth: 64,
       frameHeight: 64,
-    })
-    this.load.spritesheet('b', 'assets/b.png', {
+    });
+
+    this.load.spritesheet("ligar", "assets/ligar.png", {
       frameWidth: 64,
       frameHeight: 64,
-    })
+    });
   }
 
-  create () {
-    this.sino = this.sound.add('sino')
+  create() {
+    this.sino = this.sound.add("sino");
 
-    this.mqttClient = mqtt.connect('wss://itl.sj.ifsc.edu.br/mqtt/')
+    this.mqttClient = mqtt.connect("wss://itl.sj.ifsc.edu.br/mqtt/");
 
-    this.mqttClient.on('connect', (err) => {
-      console.log('Conectado ao broker MQTT')
-      this.mqttClient.subscribe('itl20242/res/#', (err) => {
+    this.mqttClient.on("connect", (err) => {
+      console.log("Conectado ao broker MQTT");
+      this.mqttClient.subscribe("itl20242/res/#", (err) => {
         if (!err) {
-          console.log('Tópico assinado')
+          console.log("Tópico assinado");
         }
-      })
-    })
+      });
+    });
 
-    this.mqttClient.on('message', (topic, message) => {
-      this.sino.play()
-      console.log(topic, message.toString())
-    })
+    this.mqttClient.on("message", (topic, message) => {
+      this.sino.play();
+      console.log(topic, message.toString());
+    });
+
+    this.anims.create({
+      key: "desligar",
+      frames: this.anims.generateFrameNumbers("desligar", { start: 0, end: 7 }),
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "ligar",
+      frames: this.anims.generateFrameNumbers("ligar", { start: 0, end: 7 }),
+      frameRate: 20,
+    });
 
     this.brinquedos = [
-      { x: 100, y: 100, numero: '1' },
-      { x: 250, y: 100, numero: '2' },
-      { x: 400, y: 100, numero: '3' },
-      { x: 100, y: 300, numero: '4' },
-      { x: 250, y: 300, numero: '5' },
-      { x: 400, y: 300, numero: '6' },
-      { x: 100, y: 500, numero: '7' },
-      { x: 250, y: 500, numero: '8' },
-      { x: 400, y: 500, numero: '9' },
-      { x: 100, y: 700, numero: '10' },
-      { x: 250, y: 700, numero: '11' },
-      { x: 400, y: 700, numero: '12' },
-    ]
-
-    this.anims.create({
-      key: 'a',
-      frames: this.anims.generateFrameNumbers('a', { start: 0, end: 7 }),
-      frameRate: 20
-    })
-
-    this.anims.create({
-      key: 'b',
-      frames: this.anims.generateFrameNumbers('b', { start: 0, end: 7 }),
-      frameRate: 20
-    })
+      {
+        x: 300,
+        y: 200,
+        imagem: "barco-viking",
+        numero: 1,
+      },
+      {
+        x: 300,
+        y: 450,
+        imagem: "carrossel",
+        numero: 2,
+      },
+      {
+        x: 300,
+        y: 700,
+        imagem: "montanha-russa",
+        numero: 3,
+      },
+    ];
 
     this.brinquedos.forEach((brinquedo) => {
-      this.add.text(brinquedo.x, brinquedo.y, brinquedo.numero, {
-        fontSize: '32px',
-        fill: '#000000',
-      })
-      brinquedo.comandos = ['a', 'b']
-      brinquedo.comandos.forEach((i) => {
-        let num
-        i === 'a' ? num = 0 : num = 1
-        brinquedo.comandos[i] = this.physics.add.sprite(brinquedo.x + 32, brinquedo.y + (num + 1) * 64, i, 0)
-          .setInteractive()
-          .on('pointerdown', () => {
-            brinquedo.comandos[i].anims.play(i)
-            this.mqttClient.publish(
-              'itl20242/req/' + brinquedo.numero,
-              num.toString(),
+      brinquedo.objeto = this.add
+        .sprite(brinquedo.x, brinquedo.y, brinquedo.imagem)
+        .setInteractive()
+        .on("pointerdown", () => {
+          if (this.desligar) this.desligar.destroy();
+          this.desligar = this.add
+            .sprite(
+              brinquedo.x - brinquedo.objeto.width / 2 + 32,
+              brinquedo.y + brinquedo.objeto.height,
+              "desligar",
             )
-          })
-      })
-    })
+            .setInteractive()
+            .on("pointerdown", () => {
+              this.desligar.play("desligar");
+              this.mqttClient.publish("itl20242/req/" + brinquedo.numero, "0");
+            });
+
+          if (this.ligar) this.ligar.destroy();
+          this.ligar = this.add
+            .sprite(
+              brinquedo.x - brinquedo.objeto.width / 2 + 96,
+              brinquedo.y + brinquedo.objeto.height,
+              "ligar",
+            )
+            .setInteractive()
+            .on("pointerdown", () => {
+              this.ligar.play("ligar");
+              this.mqttClient.publish("itl20242/req/" + brinquedo.numero, "1");
+            });
+        });
+    });
   }
 
-  update () { }
+  update() {}
 }
