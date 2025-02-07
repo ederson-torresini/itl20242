8a43f34bb110df61881e8bc280d6b3a98c131698
Ederson Torresini <etorresini@ifsc.edu.br>
2025-02-07 13:27:05 -0300

    Inverter botões liga/desliga

--- parque-de-diversoes/painel-de-controle/parque.js
+++ parque-de-diversoes/painel-de-controle/parque.js
@@ -2,8 +2,8 @@ export default class parque extends Phaser.Scene {
   constructor() {
     super("parque");
 
-    this.desligar = undefined;
     this.ligar = undefined;
+    this.desligar = undefined;
   }
 
   preload() {
@@ -15,12 +15,12 @@ export default class parque extends Phaser.Scene {
     this.load.image("roda-gigante", "assets/roda-gigante.png");
     this.load.image("chapéu-mexicano", "assets/chapéu-mexicano.png");
 
-    this.load.spritesheet("desligar", "assets/desligar.png", {
+    this.load.spritesheet("ligar", "assets/ligar.png", {
       frameWidth: 64,
       frameHeight: 64,
     });
 
-    this.load.spritesheet("ligar", "assets/ligar.png", {
+    this.load.spritesheet("desligar", "assets/desligar.png", {
       frameWidth: 64,
       frameHeight: 64,
     });
@@ -46,14 +46,14 @@ export default class parque extends Phaser.Scene {
     });
 
     this.anims.create({
-      key: "desligar",
-      frames: this.anims.generateFrameNumbers("desligar", { start: 0, end: 7 }),
+      key: "ligar",
+      frames: this.anims.generateFrameNumbers("ligar", { start: 0, end: 7 }),
       frameRate: 20,
     });
 
     this.anims.create({
-      key: "ligar",
-      frames: this.anims.generateFrameNumbers("ligar", { start: 0, end: 7 }),
+      key: "desligar",
+      frames: this.anims.generateFrameNumbers("desligar", { start: 0, end: 7 }),
       frameRate: 20,
     });
 
@@ -95,23 +95,10 @@ export default class parque extends Phaser.Scene {
         .sprite(brinquedo.x, brinquedo.y, brinquedo.imagem)
         .setInteractive()
         .on("pointerdown", () => {
-          if (this.desligar) this.desligar.destroy();
-          this.desligar = this.add
-            .sprite(
-              brinquedo.x - 32,
-              brinquedo.y + brinquedo.objeto.height,
-              "desligar",
-            )
-            .setInteractive()
-            .on("pointerdown", () => {
-              this.desligar.play("desligar");
-              this.mqttClient.publish("itl20242/req/" + brinquedo.numero, "0");
-            });
-
           if (this.ligar) this.ligar.destroy();
           this.ligar = this.add
             .sprite(
-              brinquedo.x + 32,
+              brinquedo.x - 32,
               brinquedo.y + brinquedo.objeto.height,
               "ligar",
             )
@@ -119,6 +106,19 @@ export default class parque extends Phaser.Scene {
             .on("pointerdown", () => {
               this.ligar.play("ligar");
               this.mqttClient.publish("itl20242/req/" + brinquedo.numero, "1");
+            });
+
+          if (this.desligar) this.desligar.destroy();
+          this.desligar = this.add
+            .sprite(
+              brinquedo.x + 32,
+              brinquedo.y + brinquedo.objeto.height,
+              "desligar",
+            )
+            .setInteractive()
+            .on("pointerdown", () => {
+              this.desligar.play("desligar");
+              this.mqttClient.publish("itl20242/req/" + brinquedo.numero, "0");
             });
         });
     });
