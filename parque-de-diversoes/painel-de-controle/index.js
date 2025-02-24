e998096c440e1bc1a58e42567eb45f21eebb2652
Ederson Torresini <etorresini@ifsc.edu.br>
2025-02-24 20:34:16 -0300

    Aplicar ESLint

--- parque-de-diversoes/painel-de-controle/index.js
+++ parque-de-diversoes/painel-de-controle/index.js
@@ -2,7 +2,7 @@ import config from "./config.js";
 import parque from "./parque.js";
 
 class Game extends Phaser.Game {
-  constructor() {
+  constructor () {
     super(config);
 
     this.scene.add("parque", parque);
